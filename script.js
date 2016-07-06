64d7486bf19e53d6054ca6adbae5245bb5fb840b
purcelllj <purcelllj@gmail.com>
2016-07-06 06:54:43 -0700

    creating gh-pages

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    var c = new Image();
+
+    c.onload = function() {
+        $('body').css({'background':'url("img/hawSunset.png")', 'background-repeat':'no-repeat', 'background-size':'cover', 'background-position':'center', 'background-attachment':'scroll'});
+    }
+
+    c.src = 'img/hawSunset.png';
+
+    $('input:checkbox').click(function(){
+        $(this).next().toggleClass('completed').find('.check').prop('checked',$(this).hasClass('completed'));
+    });
+
+    $('a.clearCom').on('click', function(){
+        $(this).parents('li').slideUp('fast');
+    });
+
+
+})
+
+
+//http://1.bp.blogspot.com/-UGQYEKQwjlY/VZRWQXZ3YiI/AAAAAAABJyM/9FgHq_8bCP8/s1600/06_sunset%2Bin%2Bisland_01.jpg
